5178229b5fcaf779a973241f22fea7cc4c3dd621
janggalman <janggalman@gmail.com>
2020-05-30 16:10:34 +0900

    Student에 나이 구하는 기능 추가 + getView에서 활용

--- app/src/main/java/kr/tjoeun/listview_20200530/datas/Student.kt
+++ app/src/main/java/kr/tjoeun/listview_20200530/datas/Student.kt
@@ -5,5 +5,9 @@ class Student(inputName: String, iby: Int, gender: Boolean ) {
     val name = inputName //이름이 기본적으로 String이 되도록 초기화
     val birthYear = iby  // 생년은 기본으로 Int가 되게
     val isMale = gender  // 성별을 boolean을 true :남성 / false : 여성
-    
+
+//    2020년에 1099년생은 몇살?? 2020-1988 +1 = 33
+    fun getKoreanAge(year: Int) : Int {
+        return year - this.birthYear + 1
+    }
 }
