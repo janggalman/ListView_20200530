8af350269c8775604789d4ae338d9a258d226bea
janggalman <janggalman@gmail.com>
2020-05-30 14:00:39 +0900

    XML에 리스트뷰 배치 + 목록으로 뿌려줄 데이터 클래스 생성 - datas 패키지

---
+++ app/src/main/java/kr/tjoeun/listview_20200530/datas/Student.kt
@@ -0,0 +1,9 @@
+package kr.tjoeun.listview_20200530.datas
+
+class Student {
+    
+    val name = "미입력" //이름이 기본적으로 String이 되도록 초기화
+    val birthYear = 0  // 생년은 기본으로 Int가 되게
+    val isMale = true  // 성별을 boolean을 true :남성 / false : 여성
+    
+}
