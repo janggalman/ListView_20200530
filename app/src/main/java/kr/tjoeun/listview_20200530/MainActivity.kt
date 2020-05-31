07eb560b31ff83f23bbe74259d311018ddda3c0c
janggalman <janggalman@gmail.com>
2020-05-31 10:07:33 +0900

    학생정보조회 추가

--- app/src/main/java/kr/tjoeun/listview_20200530/MainActivity.kt
+++ app/src/main/java/kr/tjoeun/listview_20200530/MainActivity.kt
@@ -26,9 +26,29 @@ class MainActivity : AppCompatActivity() {
         students.add(Student("엄곤지", 1984, false))
         students.add(Student("이승원", 1978, true))
         students.add(Student("이현호", 1981, true))
+        students.add(Student("조경진", 1988, true))
+        students.add(Student("김광철", 1966, true))
+        students.add(Student("김재환", 1993, true))
+        students.add(Student("박수정", 1994, false))
+        students.add(Student("신용성", 1988, true))
+        students.add(Student("엄곤지", 1984, false))
+        students.add(Student("이승원", 1978, true))
+        students.add(Student("이현호", 1981, true))
+        students.add(Student("조경진", 1988, true))
+        students.add(Student("김광철", 1966, true))
+        students.add(Student("김재환", 1993, true))
+        students.add(Student("박수정", 1994, false))
+        students.add(Student("신용성", 1988, true))
+        students.add(Student("엄곤지", 1984, false))
+        students.add(Student("이승원", 1978, true))
+        students.add(Student("이현호", 1981, true))
 
         studentAdapter = StudentAdapter(this, R.layout.student_list_item, students)
 
         studentListView.adapter =studentAdapter
+
+        studentListView.setOnItemClickListener { parent, view, position, id ->
+
+        }
     }
 }
