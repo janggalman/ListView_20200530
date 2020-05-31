401704e28de2d9575c5b42a041adc69bc8c6532e
janggalman <janggalman@gmail.com>
2020-05-31 10:32:25 +0900

    실제 학생 데이터를 연결

--- app/src/main/java/kr/tjoeun/listview_20200530/ViewStudentActivity.kt
+++ app/src/main/java/kr/tjoeun/listview_20200530/ViewStudentActivity.kt
@@ -2,6 +2,7 @@ package kr.tjoeun.listview_20200530
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_view_student.*
 import kr.tjoeun.listview_20200530.datas.Student
 
 class ViewStudentActivity : AppCompatActivity() {
@@ -11,6 +12,17 @@ class ViewStudentActivity : AppCompatActivity() {
         setContentView(R.layout.activity_view_student)
 
         val student = intent.getSerializableExtra("student") as Student
+
+        nameTxt.text = student.name
+        ageTxt.text = "${student.getKoreanAge(2020)}세(${student.birthYear})}"
+
+         if (student.isMale)
+         {
+             genderTxt.text = "남성"
+         }
+         else {
+             genderTxt.text = "여성"
+         }
 
     }
 }
